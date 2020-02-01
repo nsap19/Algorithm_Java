20cd13f8153fbb4bcd3b9b13c11d3da65d6778c0
nsap19 <zzz0484@naver.com>
2020-02-01 23:18:38 +0900

    Baekjoon4344 수정

--- src/baekjoon_step6/Baekjoon4344.java
+++ src/baekjoon_step6/Baekjoon4344.java
@@ -9,29 +9,30 @@ public class Baekjoon4344 {
         int c = sc.nextInt(); //케이스 수
         int n; //학생수
         int[] score; //각 케이스별 점수를 갖는 배열
-        int[] sc_sum = new int[c];//각 케이스별 점수 합계 배열
-        double avg[] = new double[c]; //각 케이스별 점수 평균 배열
-        double[] over_student = new double[c]; //각 케이스별 평균을 넘는 학생의 수 배열
+        double avg;
         double[] ratio = new double[c]; //각 케이스별 평균을 넘는 학생의 비율 배열
 
         for (int i = 0; i < c; i++) {
             n = sc.nextInt();
             score = new int[n];
+            int sc_sum = 0; //점수 합계
+            double over_student = 0; //평균을 넘는 학생의 수
+
 
             for (int j = 0; j < score.length; j++) {
                 score[j] = sc.nextInt(); //각 점수를 입력받아 score배열에 저장
-                sc_sum[i] += score[j];
+                sc_sum += score[j];
             }
 
-            avg[i] = sc_sum[i] / n; //한 케이스의 평균 구함
+            avg = sc_sum / n; //한 케이스의 평균
 
             for (int j = 0; j < n; j++) {
-                if (score[j] > avg[i]) {
-                    over_student[i]++; //평균을 넘는 학생수 구함
+                if (score[j] > avg) {
+                    over_student++; //평균을 넘는 학생수 구함
                 }
             }
 
-            ratio[i] = over_student[i] / n * 100; //평균을 넘는 학생 수 비율 구함
+            ratio[i] = over_student / n * 100; //평균을 넘는 학생 수 비율 구함
         }
 
         for (int i = 0; i < c; i++) {
