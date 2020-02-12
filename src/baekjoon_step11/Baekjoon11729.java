4921726a4ccf293138e21a596ad09928ae42069e
nsap19 <zzz0484@naver.com>
2020-02-13 01:20:50 +0900

    Feb13

--- src/baekjoon_step11/Baekjoon11729.java
+++ src/baekjoon_step11/Baekjoon11729.java
@@ -1,7 +1,16 @@
 package baekjoon_step11;
 
-public class Baekjoon11729 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Baekjoon11729 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); //원판의 갯수
+    }
+
+    public static String hanoi(int n){
+        String result = "";
+        return result;
     }
 }
