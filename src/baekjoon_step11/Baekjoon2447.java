12a0bbb049fd2bd6b48e24354fa4b3d9aa98a63a
nsap19 <zzz0484@naver.com>
2020-02-14 15:36:23 +0900

    Feb14

--- src/baekjoon_step11/Baekjoon2447.java
+++ src/baekjoon_step11/Baekjoon2447.java
@@ -18,10 +18,9 @@ public class Baekjoon2447 {
     }
 
     public static String[][] star(int n) {
-        String[][] arr;
+        String[][] arr = new String[n][n];
 
         if (n == 3) {
-            arr = new String[3][3];
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     if (i == 1 && j == 1) {
@@ -31,12 +30,11 @@ public class Baekjoon2447 {
             }
             return arr;
         } else {
-            arr = new String[n/3][n/3] ;
-            for (int i = 0; i < n/3; i++) {
-                for (int j = 0; j < n/3; j++) {
-                    if (i % 3 == 0 && j % 3 == 0) {
-                        arr[i][j] = "";
-                    } else arr = star(n / 3);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i % 3 == 1 && j % 3 == 1) {
+                        arr[i][j] = " ";
+                    } else arr[i][j] = star(n/3)[i/3][j/3];
                 }
             }
         }
