5a530981c10396f58ba50cbb1633ee5e2157489e
nsap19 <zzz0484@naver.com>
2020-02-15 01:17:38 +0900

    Feb15

--- src/baekjoon_step11/Baekjoon11729.java
+++ src/baekjoon_step11/Baekjoon11729.java
@@ -1,16 +1,35 @@
+/* 원반이 n개 일때
+1번 기둥에 있는 n-1개 원반을 2번 기둥에 옮긴다. -1
+1번 기둥에 가장 큰 원반을 3번 기둥으로 옮긴다. -2
+2번 기둥에 있는 n-1개 원반을 3번 기둥으로 옮긴다. -3
+ */
 package baekjoon_step11;
 
 import java.util.Scanner;
 
 public class Baekjoon11729 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static int count = 0;
+    public static StringBuilder result = new StringBuilder(); //그냥 string으로 연산하면 시간초과가 뜨기때문에 stringbuilder를 사용
 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //원판의 갯수
+        hanoi(n, 1, 2, 3);
+        result.insert(0, count+"\n");
+        System.out.println(result);
+
     }
 
-    public static String hanoi(int n){
-        String result = "";
-        return result;
+    public static void hanoi(int n, int from, int by, int to) {
+        if (n == 1) { // -2
+            count++;
+            result.append(from + " " + to + "\n");
+        } else {
+            hanoi(n - 1, from, to, by); // -1
+            result.append(from + " " + to + "\n");
+            count++;
+            hanoi(n - 1, by, from, to); // -3
+        }
     }
 }
--- src/baekjoon_step11/Baekjoon2447.java
+++ src/baekjoon_step11/Baekjoon2447.java
@@ -3,29 +3,34 @@ package baekjoon_step11;
 import java.util.Scanner;
 
 public class Baekjoon2447 {
+    public static char[][] arr;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        String[][] result = star(n);
+//        char[][] result = star(n);
+
+//        if (n != 3) n /= 3;
+        arr = new char[n][n];
+
+        star(n);
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(result[i][j]);
-            }
+            System.out.print(arr[i]);
             System.out.println();
         }
     }
 
-    public static String[][] star(int n) {
-        String[][] arr = new String[n][n];
+    public static char[][] star(int n) {
+//        arr = new char[n][n];
 
         if (n == 3) {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     if (i == 1 && j == 1) {
-                        arr[i][j] = " ";
-                    } else arr[i][j] = "*";
+                        arr[i][j] = ' ';
+                    } else arr[i][j] = '*';
                 }
             }
             return arr;
@@ -33,11 +38,12 @@ public class Baekjoon2447 {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     if (i % 3 == 1 && j % 3 == 1) {
-                        arr[i][j] = " ";
-                    } else arr[i][j] = star(n/3)[i/3][j/3];
+                        arr[i][j] = ' ';
+                    } else arr[i][j] = star(n / 3)[i/3][j/3];
                 }
             }
         }
+
         return arr;
     }
 }
