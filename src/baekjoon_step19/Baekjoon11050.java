4645f2af41353929410de8e966228a97dc466669
nsap19 <zzz0484@naver.com>
2020-04-11 21:01:27 +0900

    Apr11

---
+++ src/baekjoon_step19/Baekjoon11050.java
@@ -0,0 +1,20 @@
+package baekjoon_step19;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon11050 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+
+
+    }
+
+//    static void Factorial()
+}
