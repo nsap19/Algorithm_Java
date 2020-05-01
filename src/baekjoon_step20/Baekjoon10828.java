326b111d901084062b8c99a4b6ebea760d3598df
nsap19 <zzz0484@naver.com>
2020-05-02 01:30:00 +0900

    May 2

--- src/baekjoon_step20/Baekjoon10828.java
+++ src/baekjoon_step20/Baekjoon10828.java
@@ -18,7 +18,7 @@ public class Baekjoon10828 {
             if (str.equals("push")) {
                 int v = Integer.parseInt(st.nextToken());
                 stack.push(v);
-            } else if (str.equals("pop")) {
+            } else if (str.equals("pop")) { //스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력
                 if (stack.size() == 0)
                     bw.write("-1\n");
                 else
@@ -31,7 +31,7 @@ public class Baekjoon10828 {
                 if (stack.size() == 0)
                     bw.write("-1\n");
                 else
-                    bw.write(String.valueOf(stack.peek())+"\n");
+                    bw.write(String.valueOf(stack.peek())+"\n"); //스택의 가장 위에 있는 정수를 출력
             }
 
             bw.flush();
