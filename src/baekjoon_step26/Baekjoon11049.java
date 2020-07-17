3219fb0c1a107ab78905c9479000c635907428e6
nsap19 <zzz0484@naver.com>
2020-07-17 21:20:49 +0900

    July 17

--- src/baekjoon_step26/Baekjoon11049.java
+++ src/baekjoon_step26/Baekjoon11049.java
@@ -9,27 +9,29 @@ public class Baekjoon11049 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        int[][] a = new int[n][2];
         int[][] dp = new int[n][n];
 
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            a[i][0] = Integer.parseInt(st.nextToken());
+            a[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < arr.length ; i++) {
-            System.out.print(arr[i]+" ");
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                dp[j][j + i] = Integer.MAX_VALUE;
+                for (int k = 0; k < i; k++) {
+                    int cost = dp[j][j + k] + dp[j + k + 1][j + i] + a[j][0] * a[j + k][1] * a[j + i][1];
+//                    System.out.println(i+" "+j+" "+k);
+//                    System.out.println(cost+" "+dp[j][j+1]);
+                    dp[j][j + i] = Math.min(dp[j][j + i], cost);
+                }
+            }
         }
+        bw.write(dp[0][n-1] + "\n");
 
-        for (int i = 1; i <= n ; i++)
-            dp[i][i] =0;
-
-
-
-
-            bw.write(dp[1][n]+"\n");
-
-            bw.close();
+        bw.close();
     }
 }
