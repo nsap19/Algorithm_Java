b9436d1755f87e64c73738d6f3108f9e3d6a9d25
nsap19 <zzz0484@naver.com>
2020-08-15 12:03:52 +0900

    Aug 15

--- src/etc/CodingTest3.java
+++ src/etc/CodingTest3.java
@@ -1,38 +1,39 @@
 package etc;
 
 import java.io.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class CodingTest3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String answer = "";
         String[] banks = br.readLine().split(" ");
 
-        for (int i = 0; i < banks.length; i++) { //끝지점
-            Stack<String> bank = new Stack<>();
 
-            if (i < 5) {
-                for (int j = 0; j <= i; j++) { //시작 지점
-                    bank.push(banks[j]);
-                    System.out.println(j+" ~ "+i);
-                }
+        for (int i = 0; i < banks.length; i++) {
+            Stack<String> stack = new Stack<>();
+
+            for (int j = 0; j < i + 1; j++) {
+                String bankName = banks[j];
+                stack.push(bankName);
             }
-            else {
-                for (int j = i - 5; j < i+1; j++) { //시작 지점
-//                System.out.println(banks[j]+" "+bank.search(banks[j]));
-                    bank.push(banks[j]);
-                    System.out.println(j+" ~ "+i);
+//            System.out.println(stack.toString());
+
+            Set<String> bankList = new LinkedHashSet<>();
+            while (!stack.isEmpty()) {
+                if (bankList.size() == 5) {
+                    break;
                 }
+                bankList.add(stack.pop());
             }
 
-            for (int k = 0; k < bank.size(); k++) {
-                if (!answer.contains(bank.peek())) answer += bank.pop()+" ";
-            }
-            System.out.println(answer);
-            answer = "";
+            bankList.forEach(v -> {
+                System.out.print(v+" ");
+            });
+            System.out.println();
+
         }
     }
 }
