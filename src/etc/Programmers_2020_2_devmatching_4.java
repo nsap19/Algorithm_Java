235f6243197e9c746fd7bee10e7b43ea284f5333
nsap19 <zzz0484@naver.com>
2020-12-30 23:57:19 +0900

    DEC 30

--- src/etc/Programmers_2020_2_devmatching_4.java
+++ src/etc/Programmers_2020_2_devmatching_4.java
@@ -4,11 +4,11 @@ import java.io.*;
 import java.util.*;
 
 public class Programmers_2020_2_devmatching_4 {
-    static class Vote implements Comparable<Vote>{
+    static class Vote implements Comparable<Vote> {
         String name;
         int count;
 
-        public Vote(String name, int count){
+        public Vote(String name, int count) {
             this.name = name;
             this.count = count;
         }
@@ -22,7 +22,7 @@ public class Programmers_2020_2_devmatching_4 {
 
         @Override
         public String toString() {
-            return name + " / " + count ;
+            return name + " / " + count;
         }
     }
 
@@ -33,25 +33,54 @@ public class Programmers_2020_2_devmatching_4 {
         String[] votes = br.readLine().split(", ");
         int k = Integer.parseInt(br.readLine());
 
-        //기업명과 득표수 배열을 따로두는게 나을지도 ?
-
-        HashMap<String, Integer> vote_count = new HashMap<>();
+        //case 1 : 기업명과 득표수를 hashmap으로 표현한 후, priority queue로 정렬 - 상위 k개의 득표수 구하는데 어려움이 생
+        /*HashMap<String, Integer> vote_count = new HashMap<>();
 
         for (int i = 0; i < votes.length; i++) {
-            if(vote_count.containsKey(votes[i])){
-                vote_count.put(votes[i], vote_count.get(votes[i])+1);
-            }
-            else vote_count.put(votes[i],1);
+            if (vote_count.containsKey(votes[i])) {
+                vote_count.put(votes[i], vote_count.get(votes[i]) + 1);
+            } else vote_count.put(votes[i], 1);
         }
 
         PriorityQueue<Vote> pq = new PriorityQueue<>();
-        for(String key : vote_count.keySet()){
+        int top = 0;
+        int temp = vote_count.size();
+        for (String key : vote_count.keySet()) {
             Vote vote = new Vote(key, vote_count.get(key));
             pq.add(vote);
         }
 
-        while(!pq.isEmpty()){
-            System.out.println(pq.poll().toString());
+        temp = 0;
+        String last = "";
+        while (!pq.isEmpty()) {
+            temp += pq.peek().count;
+            last = pq.poll().name;
+            if (temp >= top) break;
+        }
+
+        bw.close();
+         */
+
+
+        //case 2김 : 기업명과 득표수의 배열을 따로 표현
+        Arrays.sort(votes);
+        ArrayList<String> c_name = new ArrayList(); //기업명
+        ArrayList<Integer> c_vote = new ArrayList(); //기업별 득표수
+
+        int v_count = 1;
+        for (int i = 1; i < votes.length; i++) {
+            if(!votes[i].equals(votes[i-1])){
+                c_name.add(votes[i-1]);
+                c_vote.add(v_count);
+                v_count=1;
+            }
+            else v_count++;
+        }
+
+//        System.out.println(c_name.size());
+//        System.out.println(c_vote.size());
+        for (int i = 0; i < c_name.size(); i++) {
+            System.out.println(c_name.get(i)+" / "+c_vote.get(i));
         }
     }
 }
