b4cc4231e4b2e2bd2bfd512543b4745f56d882fd
nsap19 <viphyemin@gmail.com>
2021-09-23 13:29:07 +0900

    0923

---
+++ src/baekjoon_silver/Baekjoon18428.java
@@ -0,0 +1,12 @@
+package baekjoon_silver;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon18428 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+    }
+}
